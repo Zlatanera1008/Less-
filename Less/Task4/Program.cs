beaa5afc1315c3effe25ea90ed304ef49803589a
Evgeni Kirichenko <zhenya_kirichenko_2000@mail.ru>
2024-04-25 21:04:17 +0300

    less:task4

---
+++ Less/Task4/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = 6;
+int b = 3;
+int c = 8;
+int d = 3;
+int e = 1;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}if (d > max)
+{
+    max = d;
+}if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
